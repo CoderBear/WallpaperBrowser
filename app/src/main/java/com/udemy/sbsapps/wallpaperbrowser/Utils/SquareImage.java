bd33f086539c2abce58c95d9f7feab2b7e8c2bfa
Joe Ortega <joeortega79@gmail.com>
2018-04-06 12:22:42 +0930

    Add a fragment for the item phot that is clicked on.  Shows user photo and user name.

---
+++ app/src/main/java/com/udemy/sbsapps/wallpaperbrowser/Utils/SquareImage.java
@@ -0,0 +1,36 @@
+package com.udemy.sbsapps.wallpaperbrowser.Utils;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+/**
+ * Created by michaelmallamo on 6/04/2018.
+ */
+
+@SuppressLint("AppCompatCustomView")
+public class SquareImage extends ImageView {
+    public SquareImage(Context context) {
+        super(context);
+    }
+
+    public SquareImage(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SquareImage(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public SquareImage(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        setMeasuredDimension(getMeasuredWidth(), getMeasuredHeight());
+    }
+}
